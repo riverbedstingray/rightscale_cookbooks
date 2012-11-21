dad9f35672890b9c0883916d8152fed9250fb637
Alex Gosse <gosse.alex@gmail.com>
2012-11-21 17:11:28 +0000

    Updated code in default recipe.

--- cookbooks/lb_stingray/recipes/default.rb
+++ cookbooks/lb_stingray/recipes/default.rb
@@ -16,13 +16,20 @@ end
 log "Override load balancer to use Stingray."
 node[:lb][:service][:provider] = "lb_stingray"
 
-vhosts(node[:lb][:vhost_names]).each do | vhost_name |
-   log "    Setup default load balancer resource for vhost '#{vhost_name}'."
-   lb vhost_name do
-      provider node[:lb][:service][:provider] = "lb_stingray"
-      persist true
-      action :nothing
-   end
+# 2D array of pools
+# Example: [["_serverid", "/serverid"], ["_appsever", "/appsever"], ["default", "default"]]
+pool_list = node[:lb][:pools].gsub(/\s+/, "").split(",").uniq.map { |pool| [ pool.gsub(/[\/]/, '_'), pool ] }
+
+pool_list.each do |pool_name_short, pool_name_full|
+  log "  Setup default load balancer resource for pool '#{pool_name_short}'."
+  log "  load balancer pool full name is '#{pool_name_full}'."
+
+  lb pool_name_short do
+    provider "lb_stingray"
+    pool_name_full pool_name_full
+    persist true # Store this resource in node between converges.
+    action :nothing
+  end
 end
 
 rightscale_marker :end
