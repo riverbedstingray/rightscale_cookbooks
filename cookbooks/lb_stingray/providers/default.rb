b0060ce0eed5556b9ab5988d27e4978dd22fe9e4
Alex Gosse <gosse.alex@gmail.com>
2012-11-22 12:00:39 +0000

    Re-fixed file detection function calls.

--- cookbooks/lb_stingray/providers/default.rb
+++ cookbooks/lb_stingray/providers/default.rb
@@ -39,7 +39,7 @@ action :install do
 
     # Replay file for non-interactive installation of Stingray.
     template "/tmp/install_replay" do
-        not_if { File.exists?("/opt/riverbed/zxtm") }
+        not_if { ::File.exists?("/opt/riverbed/zxtm") }
         cookbook "stingray"
         mode "0644"
         source "install.erb"
@@ -66,7 +66,7 @@ action :install do
     # Add RS-specific tunings.
     # FIXME: Do something about this - use zcli?
     template "/opt/riverbed/zxtm/conf/settings.cfg" do
-        not_if { File.exists?("/opt/riverbed/rc.d/S20zxtm") }
+        not_if { ::File.exists?("/opt/riverbed/rc.d/S20zxtm") }
         backup false
         cookbook "stingray"
         source "settings.erb"
